2452608e19c560e38e7a56c7dbff25de78f85bcd
Songül Yaman Bayık <snglymn@gmail.com>
2024-11-15 14:42:05 +0300

    .

---
+++ Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class ProductRepository : GenericRepository<Product>
+    {
+        private readonly Context _context;
+
+        public ProductRepository(Context context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
