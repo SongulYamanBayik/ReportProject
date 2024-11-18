ab51c8dc1b726e79bad11d07b38b583b1992dde9
Songül Yaman Bayık <snglymn@gmail.com>
2024-11-18 17:12:19 +0300

    .

---
+++ ReportProjectApi/Controllers/ProductController.cs
@@ -0,0 +1,76 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReportProjectApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IGenericRepository<Product> _productRepository;
+
+        public ProductController(IGenericRepository<Product> productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        // GET: api/company
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetAll(ISpecification<Product> spec)
+        {
+            var products = await _productRepository.GetAllAsync(spec, null, true);
+            return Ok(products);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetById(int id)
+        {
+            var product = await _productRepository.GetByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> Create([FromBody] Product product)
+        {
+            if (product == null)
+            {
+                return BadRequest();
+            }
+
+            await _productRepository.AddAsync(product);
+            return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            await _productRepository.UpdateAsync(product);
+            return NoContent(); // Güncelleme başarılı olduğunda 204 No Content döner
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var product = await _productRepository.GetByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            await _productRepository.DeleteAsync(id);
+            return NoContent(); // Silme başarılı olduğunda 204 No Content döner
+        }
+    }
+}
